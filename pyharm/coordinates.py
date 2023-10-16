9159429d196f55c1e0ef210694ab2780b752486e
Hyerin Cho <chyerin1996@gmail.com>
2023-10-16 10:58:38 -0400

    slight improvements for 2d case, and smoothness minimum

--- pyharm/coordinates.py
+++ pyharm/coordinates.py
@@ -556,13 +556,16 @@ class WKS(KS):
         self.lin_frac = met_params['lin_frac']
         self.n2 = met_params['n2']
         self.n3 = met_params['n3']
-        t = self.lin_frac / (1. - self.lin_frac) * (1. / np.pi - 1. / self.n3) * self.n3 / self.n2
+        if self.n3 < np.pi: self.n3 = self.n2
+        if self.lin_frac == 1: t = 1
+        else: t = self.lin_frac / (1. - self.lin_frac) * (1. / np.pi - 1. / self.n3) * self.n3 / self.n2
         if abs(t) >= 1.: #self.lin_frac >= np.power(1./np.pi-1./self.n2+1.,-1.):
             print("WARNING: it is hard to get del phi ~ del theta. consider using a smaller lin_frac < {:.3g}".format(np.power((1./np.pi-1./self.n2)*self.n3/self.n2+1.,-1.)))
             self.smoothness = 0.8/self.n2
         else:
             # can define smoothness to get a del phi (pole) ~ del theta (midplane). higher the number, the smoother the transition is
             self.smoothness = np.power(self.n2 * np.log((1. + t) / (1. - t)),-1.)
+        self.smoothness = max(0.01, self.smoothness)
         print(self.smoothness)
         super(WKS, self).__init__(met_params)
 
